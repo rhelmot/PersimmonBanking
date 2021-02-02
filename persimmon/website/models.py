3851b7b5fc09ba694016d84f61341529947e9b0e
Audrey Dutcher <audrey@rhelmot.io>
2021-02-01 22:02:20 -0700

    Lint files; add lint config

--- persimmon/website/models.py
+++ persimmon/website/models.py
@@ -52,7 +52,8 @@ class SignInHistory(models.Model):
 
 class Appointment(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments_as_customer')
-    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments_as_employee', limit_choices_to={'employee_level__gte': EmployeeLevel.TELLER})
+    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments_as_employee',
+            limit_choices_to={'employee_level__gte': EmployeeLevel.TELLER})
     time = models.DateTimeField()
 
     class Meta:
