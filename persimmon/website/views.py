7a6020931afbc4c285083ada55186788c06b165a
AkshayKoshy <AGK@users.noreply.github.com>
2021-02-08 14:22:34 -0700

    Update views.py

--- persimmon/website/views.py
+++ persimmon/website/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def schedule_appointment(request):
-    return HttpResponse('<h1> Appointment</h1>')
+    return HttpResponse('<h1>Schedule Appointment</h1>')
