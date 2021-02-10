df1ec911bc672d6cea063d39095e2b6f33d9f643
Audrey Dutcher <audrey@rhelmot.io>
2021-02-10 03:05:37 -0700

    lint

--- persimmon/website/tests/test_session.py
+++ persimmon/website/tests/test_session.py
@@ -6,8 +6,8 @@ from .. import views
 
 class TestSessions(TestCase):
     @staticmethod
-    def is_logged_in(c):
-        req = c.post(
+    def is_logged_in(client):
+        req = client.post(
             reverse(views.get_my_accounts),
             content_type='application/json',
             data={})
@@ -19,52 +19,52 @@ class TestSessions(TestCase):
 
     def test_login_logout(self):
         make_user('username', password='password')
-        c = Client()
+        client = Client()
 
         # login with invalid creds should fail
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_login),
             content_type='application/json',
             data={"username": "username", "password": "wrong"})
         self.assertEqual(req.status_code, 200)
         req_data = req.json()
         self.assertIn("error", req_data)
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_login),
             content_type='application/json',
             data={"username": "wrong", "password": "password"})
         self.assertEqual(req.status_code, 200)
         req_data = req.json()
         self.assertIn("error", req_data)
-        self.assertFalse(self.is_logged_in(c))
+        self.assertFalse(self.is_logged_in(client))
 
         # successful login should work
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_login),
             content_type='application/json',
             data={"username": "username", "password": "password"})
         self.assertEqual(req.status_code, 200)
         req_data = req.json()
         self.assertNotIn("error", req_data)
-        self.assertTrue(self.is_logged_in(c))
+        self.assertTrue(self.is_logged_in(client))
 
         # login should not work once you're logged in
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_login),
             content_type='application/json',
             data={"username": "username", "password": "password"})
         self.assertEqual(req.status_code, 404)
 
         # logout should work
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_logout),
             content_type='application/json',
             data={})
         self.assertEqual(req.status_code, 200)
-        self.assertFalse(self.is_logged_in(c))
+        self.assertFalse(self.is_logged_in(client))
 
         # logout should not work once you're logged out
-        req = c.post(
+        req = client.post(
             reverse(views.persimmon_logout),
             content_type='application/json',
             data={})
