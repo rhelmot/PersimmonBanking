8441c93e71e8e7b769ae0aab123d213e7556d4d3
RunlinXiao <73460722+RunlinXiao@users.noreply.github.com>
2021-02-16 15:22:48 +0800

    modify model

--- persimmon/website/models.py
+++ persimmon/website/models.py
@@ -68,7 +68,7 @@ class BankStatements(models.Model):
     date = models.DateField(auto_now=True)
     transaction = models.CharField(max_length=30)
     balance = models.DecimalField(decimal_places=2, max_digits=10)
-    accountId = models.ForeignKey(BankAccount.id, on_delete=models.CASCADE)
+    accountId = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
     approval_status = models.IntegerField(choices=ApprovalStatus.choices, default=ApprovalStatus.PENDING)
 
 
