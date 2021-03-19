43297f08067722c8fa156d692999a70bc3c4f48a
gndeshpa <gauravd257@gmail.com>
2021-03-19 13:50:16 -0700

    Started separating concerns and used some built in libraries to help with chat bot

--- persimmon/website/chatbot.py
+++ persimmon/website/chatbot.py
@@ -1,8 +1,10 @@
 from chatterbot import ChatBot, comparisons, response_selection
-from chatterbot.trainers import ListTrainer
-
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
 
 def runbot(inp):
+
+    # Setting up the basic functionality of how the chatbot should operate
     chatbot = ChatBot("Persimmon Bank",
                       logic_adapters=[
                           {
@@ -19,15 +21,19 @@ def runbot(inp):
                       ]
                       )
 
-    trainer = ListTrainer(chatbot)
-    trainer.train([
-        "Hi",
-        "http://127.0.0.1:8000/website/chatbot",
-        "Hi how's it going?",
-        "http://127.0.0.1:8000/website/chatbot",
-        "Yo",
-        "http://127.0.0.1:8000/website/chatbot"
-    ])
+    trainer = ChatterBotCorpusTrainer(chatbot)
 
+    trainer.train("chatterbot.corpus.english.greetings",
+                "chatterbot.corpus.english.conversations"
+                  )
+    trainer.train(os.path.join(os.path.dirname(__file__), './trainer/home_page.json'))
+
+
+    # "Hi",
+    # "http://127.0.0.1:8000/website/chatbot",
+    # "Hi how's it going?",
+    # "http://127.0.0.1:8000/website/chatbot",
+    # "Yo",
+    # "http://127.0.0.1:8000/website/chatbot"
     response = chatbot.get_response(inp)
     return response
