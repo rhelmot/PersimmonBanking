8d9b4b76d78510396e18ae7105ca1a3ffe79183a
gndeshpa <gauravd257@gmail.com>
2021-03-31 20:11:05 -0700

    Moved chatbot constructor to gloabl scope

--- persimmon/website/chatbot.py
+++ persimmon/website/chatbot.py
@@ -2,25 +2,26 @@ from chatterbot import ChatBot, response_selection
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
 
+# Setting up the basic functionality of how the chatbot should operate
+
+chatbot = ChatBot("Persimmon Bank",
+                  logic_adapters=[
+                      {
+                          "import_path": "chatterbot.logic.BestMatch",
+                          "statement_comparison_function": "comparisons.levenshtein_distance",
+                          "response_selection_method": response_selection.get_first_response
+                      },
+                      {
+                          "import_path": "chatterbot.logic.BestMatch",
+                          "default_response": "I'm sorry I'm not sure what you mean",
+                          "maximum_similarity_threshold": .95
+                      }
+
+                  ], database_uri=None
+                  )
+
 
 def runbot(inp):
-    # Setting up the basic functionality of how the chatbot should operate
-    chatbot = ChatBot("Persimmon Bank",
-                      logic_adapters=[
-                          {
-                              "import_path": "chatterbot.logic.BestMatch",
-                              "statement_comparison_function": "comparisons.levenshtein_distance",
-                              "response_selection_method": response_selection.get_first_response
-                          },
-                          {
-                              "import_path": "chatterbot.logic.BestMatch",
-                              "default_response": "I'm sorry I'm not sure what you mean",
-                              "maximum_similarity_threshold": .95
-                          }
-
-                      ]
-                      )
-
     trainer = ChatterBotCorpusTrainer(chatbot)
 
     trainer.train(os.path.join(os.path.dirname(__file__), './trainer/appointment.json'),
