be9c6880c8319dfedd31cb318b6ac1776855b6be
Audrey Dutcher <audrey@rhelmot.io>
2021-04-02 01:24:37 -0700

    make the chatbot less chatty

--- persimmon/website/chatbot.py
+++ persimmon/website/chatbot.py
@@ -1,6 +1,7 @@
 import os
 from chatterbot import ChatBot, response_selection
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import chatterbot.utils
 
 from django.conf import settings
 
@@ -30,7 +31,7 @@ def train_bot():
         os.remove(settings.CHATBOT_DATABASE)
 
     chat_bot = build_bot()
-    trainer = ChatterBotCorpusTrainer(chat_bot)
+    trainer = ChatterBotCorpusTrainer(chat_bot, show_training_progress=False)
 
     trainer.train(
         os.path.join(os.path.dirname(__file__), './trainer/appointment.json'),
@@ -50,4 +51,41 @@ def run_bot(inp):
     return response
 
 
+# hackfix. we need to monkeypatch download(quiet=True) for our own sanity but also so CI will work correctly
+# pylint: disable=import-outside-toplevel,raise-missing-from
+def nltk_download_corpus(resource_path):
+    """
+    Download the specified NLTK corpus file
+    unless it has already been downloaded.
+
+    Returns True if the corpus needed to be downloaded.
+    """
+    from nltk.data import find
+    from nltk import download
+    from os.path import split, sep
+    from zipfile import BadZipfile
+
+    # Download the NLTK data only if it is not already downloaded
+    _, corpus_name = split(resource_path)
+
+    if not resource_path.endswith(sep):
+        resource_path = resource_path + sep
+
+    downloaded = False
+
+    try:
+        find(resource_path)
+    except LookupError:
+        download(corpus_name, quiet=True)
+        downloaded = True
+    except BadZipfile:
+        raise BadZipfile(
+            'The NLTK corpus file being opened is not a zipfile, '
+            'or it has been corrupted and needs to be manually deleted.'
+        )
+
+    return downloaded
+
+
 train_bot()
+chatterbot.utils.nltk_download_corpus = nltk_download_corpus
