2267c9be2ba92817578dcef79c8548c0475fd3c4
Audrey Dutcher <audrey@rhelmot.io>
2021-04-03 20:41:58 +0000

    Add logging config

--- persimmon/persimmon/settings.py
+++ persimmon/persimmon/settings.py
@@ -39,6 +39,30 @@ else:
     ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "persimmon.rhelmot.io").split(',')
 
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
